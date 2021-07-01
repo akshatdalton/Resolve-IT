7f4f8bc90030ea0868ed11bbedc0c880bc2a3c8e
akshatdalton <akshat.dak@students.iiit.ac.in>
2021-07-02 04:23:37 +0530

    resolveit: Formatted with zulint(includes other packages).
    See requirements to see what other packages zulint uses
    to format the python files.

--- resolveit.py
+++ resolveit.py
@@ -1,26 +1,28 @@
 import subprocess
 import sys
-
 from subprocess import PIPE
+from typing import List
+
 from fetch_results import parse_and_get_results
 
-def display_success_message(file):
-    file = " ".join(file)
-    print(f"Congratulations! Your file: {file} ran successfully. ✨ 🍰 ✨")
 
-def get_actual_error(stderr):
+def display_success_message(file: List[str]) -> None:
+    file_name_with_path = " ".join(file)
+    print(f"Congratulations! Your file: {file_name_with_path} ran successfully. ✨ 🍰 ✨")
+
+def get_actual_error(stderr: str) -> str:
     # Most part of the errors are just stack traceback calls.
     # We just need the actual error message.
     return stderr.split('\n')[-2].strip()
 
-def show_so_links(file, links):
-    file = " ".join(file)
-    print(f"Execution of your {file} failed. 💔\n")
+def show_so_links(file: List[str], links: List[str]) -> None:
+    file_name_with_path = " ".join(file)
+    print(f"Execution of your {file_name_with_path} failed. 💔\n")
     print("Checkout some of the Stackoverflow links:")
     for ind, link in enumerate(links):
         print(f"{ind + 1}. {link}")
 
-def main():
+def main() -> None:
     language = "python3"
     # We obtain a file with its path (if provided).
     file = sys.argv[1:]
