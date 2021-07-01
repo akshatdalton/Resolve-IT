754b6d05181d9d850fe533e8c889a26b67ec5d0f
akshatdalton <akshat.dak@students.iiit.ac.in>
2021-07-02 04:24:52 +0530

    fetch_results: Formatted with Zulint.
    See previous commit and requirements.txt
    for more details.

--- fetch_results.py
+++ fetch_results.py
@@ -1,11 +1,13 @@
-from stackapi import StackAPI
-from pprint import pprint
+from typing import List
 
-def parse_and_get_results(error_msg):
+from stackapi import StackAPI
+
+
+def parse_and_get_results(error_msg: str) -> List[str]:
     SITE = StackAPI("stackoverflow")
     SITE.max_pages = 1
     results = SITE.fetch("search/advanced", sort="votes", order="desc", q=error_msg)
-    result_links = []
+    result_links: List[str] = []
     for item in results["items"]:
         if item["is_answered"]:
             result_links.append(item["link"])
