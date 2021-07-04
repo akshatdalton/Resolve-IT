2773872f8d6496df12f6510c09ea3c77bd31a060
akshatdalton <akshat.dak@students.iiit.ac.in>
2021-07-04 23:17:17 +0530

    fetch_results: Add the helper function `get_link_content`.
    See the comment in the function to get the details of this
    function.

--- resolveit/fetch_results.py
+++ resolveit/fetch_results.py
@@ -1,6 +1,7 @@
-from typing import List
+from typing import List, Optional
 
-from settings import SEARCH_ENDPOINT
+import requests
+from settings import HEADERS, SEARCH_ENDPOINT
 from stackapi import StackAPI
 
 
@@ -14,3 +15,21 @@ def parse_and_get_results(error_msg: str) -> List[str]:
             result_links.append(item["link"])
 
     return result_links
+
+
+def get_link_content(link: str) -> Optional[str]:
+    """Here we fetch the content text of the link.
+
+    Stackexchange API doesn't provide the full excerpt of any
+    questions or answers. Therefore, we need to make a request
+    to the provided link to fetch the actual content.
+    """
+    try:
+        response = requests.get(link, headers=HEADERS, timeout=10)
+    except requests.exceptions.RequestException:
+        return None
+
+    if not response.ok:
+        return None
+
+    return response.text
