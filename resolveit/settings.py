6b335cfed624cbc06a60887c1ea3605eb09b5c5b
akshatdalton <akshat.dak@students.iiit.ac.in>
2021-07-04 23:16:17 +0530

    settings: Add `VERSION` and `HEADERS`.

--- resolveit/settings.py
+++ resolveit/settings.py
@@ -1,1 +1,13 @@
 SEARCH_ENDPOINT = "search/advanced"
+
+VERSION = 1.0
+
+GITHUB_URL = "https://github.com/akshatdalton"
+
+RESOLVE_IT_USER_AGENT = (
+    "Mozilla/5.0 (compatible; ResolveIT/{version}; +{external_host})".format(
+        version=VERSION, external_host=GITHUB_URL
+    )
+)
+
+HEADERS = {"User-Agent": RESOLVE_IT_USER_AGENT}
