58e5d5e5f5e59f06de96196851b2e54b61621317
akshatdalton <akshat.dak@students.iiit.ac.in>
2021-07-21 16:22:47 +0530

    tests/test_resolveit: Add test cases to test resolveit as context managers.

---
+++ tests/fixtures/fixture3.py
@@ -0,0 +1,4 @@
+def execute_resolveit_as_context_manager() -> None:
+    a = "str"
+    b = ["str"]
+    a + b
---
+++ tests/test_resolveit.py
@@ -0,0 +1,60 @@
+import os
+import sys
+from unittest import TestCase, main, mock
+
+RESOLVEIT_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), "../")
+sys.path.append(RESOLVEIT_PATH)
+
+from fixtures.fixture3 import execute_resolveit_as_context_manager
+
+from resolveit import ResolveIT, app, debug
+
+
+class TestResolveIT(TestCase):
+    def test_resolveit_as_class_context_manager(self) -> None:
+        app.launch_interface = mock.MagicMock()
+
+        with ResolveIT():
+            execute_resolveit_as_context_manager()
+
+        app.launch_interface.assert_called_once_with("TypeError: must be str, not list")
+        app.launch_interface.reset_mock()
+
+        # Here we ensure that ResolveIT captures only the know errors.
+        with self.assertRaises(ValueError):
+            with ResolveIT():
+                a = "foo"
+                b = "bar"
+                a += b
+            raise ValueError("foo bar baz")
+
+        app.launch_interface.assert_not_called()
+
+    def test_resolveit_as_decorator_context_manager(self) -> None:
+        app.launch_interface = mock.MagicMock()
+
+        @debug
+        def resolveit_as_decorator_context_manager() -> None:
+            execute_resolveit_as_context_manager()
+
+        resolveit_as_decorator_context_manager()
+        app.launch_interface.assert_called_once_with("TypeError: must be str, not list")
+        app.launch_interface.reset_mock()
+
+        # Here we ensure that debug captures only the know errors.
+        with self.assertRaises(ValueError):
+
+            @debug
+            def simple_function() -> None:
+                a = "foo"
+                b = "bar"
+                a += b
+
+            simple_function()
+            raise ValueError("foo bar baz")
+
+        app.launch_interface.assert_not_called()
+
+
+if __name__ == "__main__":
+    main()
